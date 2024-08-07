c611da8908d02badea30fb7e4403246cb5412f58
nagashimayudi <marcilioyudi@gmail.com>
2024-08-07 20:38:33 -0300

    Update app.py

--- app.py
+++ app.py
@@ -4,21 +4,31 @@ def finalizar_app():
     os.system('clear')
     print('Encerrando...\n')
 
-print('Ⓢⓐⓑⓞⓡ Ⓔⓧⓟⓡⓔⓢⓢ\n')
+def exibir_nome_programa():
+    print('Ⓢⓐⓑⓞⓡ Ⓔⓧⓟⓡⓔⓢⓢ\n')
 
-print('1. Cadastrar restaurante')
-print('2. Listar restaurante')
-print('3. Ativar restaurante')
-print('4. Sair\n')
+def exibir_opcoes():
+    print('1. Cadastrar restaurante')
+    print('2. Listar restaurante')
+    print('3. Ativar restaurante')
+    print('4. Sair\n')
 
-opcao_escolhida = int(input('Escolha uma opção: '))
-#opcao_escolhida = int(opcao_escolhida)
+def escolher_opcao():
+    opcao_escolhida = int(input('Escolha uma opção: '))
+    #opcao_escolhida = int(opcao_escolhida)
+    if opcao_escolhida == 1:
+        print('Cadastrar restaurante')
+    elif opcao_escolhida == 2:
+        print('Listar restaurante')
+    elif opcao_escolhida == 3:
+        print('Ativar restaurante')
+    else:
+        finalizar_app()
 
-if opcao_escolhida == 1:
-    print('Cadastrar restaurante')
-elif opcao_escolhida == 2:
-    print('Listar restaurante')
-elif opcao_escolhida == 3:
-    print('Ativar restaurante')
-else:
-    finalizar_app()
+def main():
+    exibir_nome_programa()
+    exibir_opcoes()
+    escolher_opcao()
+
+if __name__ == '__main__':
+    main()
